ccb6b18156b579ae881ade705534e1cb3872b33a
Uwe Loydl <uloydl@team.mobile.de>
2015-04-16 09:33:30 +0200

    added procfile

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ function start() {
 }
 
 function startServer() {
-    var server = app.listen(3000, function () {
+    var server = app.listen(process.env.PORT || 5000, function () {
         var host = server.address().address;
         var port = server.address().port;
 
