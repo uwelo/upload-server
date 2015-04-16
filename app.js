5e875702e783bc9aa50f50a7881fb5594d4ddcf3
Uwe Loydl <uloydl@team.mobile.de>
2015-04-16 16:40:40 +0200

    bla

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.all("/", function(req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
